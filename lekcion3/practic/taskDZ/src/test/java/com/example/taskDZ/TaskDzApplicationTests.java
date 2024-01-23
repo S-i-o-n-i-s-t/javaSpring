a07e5d4e7253f6bc50a087806c3fa26e73344359
S-i-o-n-i-s-t <metalljan@yandex.ru>
2024-01-23 14:10:30 +0300

    dz3v2

---
+++ lekcion3/practic/taskDZ/src/test/java/com/example/taskDZ/TaskDzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskDZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
